3f4a534f9bc3ce13daf32d52d94cc93cec3cf444
nfo0019nfosoft <nfo0019nfosoft@gmail.com>
2026-03-23 10:43:01 +0530

    first commit

---
+++ app/sitemap.ts
@@ -0,0 +1,118 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://rvilabs.vercel.app/",
+      lastModified: new Date(),
+    },
+
+    // Main Pages
+    {
+      url: "https://rvilabs.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rvilabs.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rvilabs.vercel.app/industries",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rvilabs.vercel.app/insights",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rvilabs.vercel.app/contact",
+      lastModified: new Date(),
+    },
+
+    // Top Links
+    {
+      url: "https://rvilabs.vercel.app/blog",
+    },
+    {
+      url: "https://rvilabs.vercel.app/careers",
+    },
+    {
+      url: "https://rvilabs.vercel.app/news",
+    },
+    {
+      url: "https://rvilabs.vercel.app/investors",
+    },
+
+    // Insights Sub Pages
+    {
+      url: "https://rvilabs.vercel.app/insights/hot-topics",
+    },
+    {
+      url: "https://rvilabs.vercel.app/insights/conversations-for-tomorrow",
+    },
+    {
+      url: "https://rvilabs.vercel.app/insights/world-economic-forum",
+    },
+    {
+      url: "https://rvilabs.vercel.app/insights/expert-perspectives",
+    },
+
+    // Industries
+    {
+      url: "https://rvilabs.vercel.app/industries/banking",
+    },
+    {
+      url: "https://rvilabs.vercel.app/industries/healthcare",
+    },
+    {
+      url: "https://rvilabs.vercel.app/industries/manufacturing",
+    },
+    {
+      url: "https://rvilabs.vercel.app/industries/retail",
+    },
+
+    // Services
+    {
+      url: "https://rvilabs.vercel.app/services/digitaltransformation",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/cloudservices",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/data-ai",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/cybersecurity",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/applicationdevelopment",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/enterpriseplatform",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/itconsulting",
+    },
+    {
+      url: "https://rvilabs.vercel.app/services/infrastructure",
+    },
+
+    // About
+    {
+      url: "https://rvilabs.vercel.app/about/who-we-are",
+    },
+    {
+      url: "https://rvilabs.vercel.app/about/leadership",
+    },
+    {
+      url: "https://rvilabs.vercel.app/about/ourvalues",
+    },
+    {
+      url: "https://rvilabs.vercel.app/about/sustainability",
+    },
+    {
+      url: "https://rvilabs.vercel.app/about/diversity-inclusion",
+    },
+    {
+      url: "https://rvilabs.vercel.app/about/corporate-responsibility",
+    },
+  ];
+}
